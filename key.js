d1cb59960401bb4d0e87573ee9ae54324a35135e
Arunava <learningdroidarunava@gmail.com>
2017-07-16 22:21:10 +0530

    Improvments to key.js

--- key.js
+++ key.js
@@ -1,42 +1,62 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, ipcMain} = require('electron');
 const path = require('path');
 const url = require('url');
 
 let win;
 let authorizationWin;
 
-function createWindow () {
+function createAuthWindow () {
 	
-	win = new BrowserWindow ({width: 800, height: 600});
-
 	authorizationWin = new BrowserWindow ({ width: 550, 
 						height: 200, 
-						parent: win,
-						resizable: false,
+						resizable: true,
 						movable: false});
 
 	authorizationWin.loadURL(url.format ({
-		pathname: path.join(__dirname, '/public/html/authorization.html'),
+		pathname: path.join(__dirname, '/lib/html/authorization.html'),
 		protocol: 'file:',
 		slashes: true
 	}));
+
 	authorizationWin.once ('ready-to-show', () => {
 		
 		authorizationWin.show();
 	});
-	win.loadURL(url.format ({
-		pathname: path.join(__dirname, '/public/html/index.html'),
-		protocol: 'file:',
-		slashes: true
+
+
+};
+
+function createMainWin () {
+	
+	win = new BrowserWindow ({width: 800, height: 600});
+
+	win.loadURL (url.format ({
+				pathname: path.join (__dirname, "../html/index.html"),
+				protocol: 'file',
+				slashes: true
+	
 	}));
 
-	win.on('closed', () => {
-		win = null;
+	win.once ('ready-to-show', () => {
+		win.show();
 	});
 
-}
+	win.on ('closed', () => {
+		win = null;
+	});
+};
 
-app.on('ready', createWindow);
+ipcMain.on ('master-password-verified', function () {
+	
+	if (!win) {
+		createMainWin();
+	}
+	authorizationWin.close();
+
+	return (!win.isDestroyed() && !win.isVisible()) ? win.hide() : win.show();
+});
+
+app.on('ready', createAuthWindow);
 
 app.on('window-all-closed', () => {
 	if (process.platform != 'darwin') {
@@ -45,7 +65,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-	if (win == null) {
-		createWindow();
+	if (authorizationWin == null) {
+		createAuthWindow();
 	}
 });
